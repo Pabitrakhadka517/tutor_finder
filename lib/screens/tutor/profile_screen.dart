1235f147c112a9d158b928459dbf173e721ed337
Pabitrakhadka517 <pratimakhadka517@gmail.com>
2025-12-15 17:13:02 +0545

    Tutor Dashboard Done

---
+++ lib/screens/tutor/profile_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TutorProfileScreen extends StatelessWidget {
+  const TutorProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Tutor Profile Screen'));
+  }
+}
---
+++ lib/screens/tutor/subject_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TutorSubjectScreen extends StatelessWidget {
+  const TutorSubjectScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Tutor Subjects Screen'));
+  }
+}
